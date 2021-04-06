a62e6a716d5043dc86985996d57662b32469ac03
Carolyn Shasha <35541083+cshasha@users.noreply.github.com>
2021-04-06 14:32:24 -0700

    Update superscan.py

--- superscan.py
+++ superscan.py
@@ -19,7 +19,7 @@ opt = parser.parse_args()
 features = pd.read_csv('features.csv',index_col=0)['0']
 model_l1 = load('model_l1.joblib.z')
 model_l2 = load('model_l2.joblib.z')
-    
+
 print('..loading data')
 
 if not os.path.exists(opt.dataset):
@@ -28,51 +28,51 @@ if not os.path.exists(opt.dataset):
 if (opt.dataset[-4:] != '.csv') & (opt.dataset[-5:] != '.h5ad'):
     sys.exit('Error: Dataset format must be either csv or h5ad.')
 
-    
+
 if opt.dataset[-4:] == '.csv':
     data = pd.read_csv(opt.dataset, index_col=0)
-    
+
     if 'CD4' in data.index:
         data = data.T
 
     overlap = list(set(data.columns) & set(features))
-    
+
     if len(overlap) < 100:
         sys.exit('Error: Not enough feature overlap.')
-        
+
     extra = list(np.setdiff1d(features,overlap))
     data = data[overlap]
     data[extra] = np.nan
     data = data[features]
-        
-if opt.dataset[-5:] == '.h5ad': 
+
+if opt.dataset[-5:] == '.h5ad':
     data = sc.read(opt.dataset)
     colnames = data.var.index
     rownames = data.obs.index
-    
+
     overlap = list(set(colnames) & set(features))
     if len(overlap) < 100:
         sys.exit('Error: Not enough feature overlap.')
-    
+
     if (type(data.X) == csc_matrix) | (type(data.X) == csr_matrix):
         data = data[:,overlap]
         data = pd.DataFrame.sparse.from_spmatrix(data.X)
     else:
         data = data[:,overlap]
         data = pd.DataFrame(data.X)
-        
+
     data.columns = overlap
     data.index = rownames
     extra = list(np.setdiff1d(features,overlap))
     data[extra] = np.nan
     data = data[features]
-    
-print('...predicting') 
+
+print('...predicting')
 
 Y_pred1 = model_l1.predict(data)
 Y_pred2 = model_l2.predict(data)
 
-print('....calculating entropy scores') 
+print('....calculating entropy scores')
 
 probs1 = model_l1.predict_proba(data)
 ent1 = entropy(probs1, base=2,axis=1)
@@ -98,7 +98,7 @@ for x in pd.Series(Y_pred1).unique():
 
 print('.....exporting results')
 
-pd.DataFrame(data={'cell':data.index,'broad label':Y_pred1, 'normalized entropy score, broad': ent1, 'confidence, broad': con1,
-                   'fine label':Y_pred2, 'normalized entropy score, fine': ent2,'confidence, fine': con2}).to_csv(opt.out_prefix + '_superscan.csv',index=False)
+pd.DataFrame(data={'cell':data.index,'broad_label':Y_pred1, 'entropy_score_broad': ent1, 'confidence_broad': con1,
+                   'fine_label':Y_pred2, 'entropy_score_fine': ent2,'confidence_fine': con2}).to_csv(opt.out_prefix + '_superscan.csv',index=False)
 print('......done. Results saved to '+opt.out_prefix+'_superscan.csv')
 print(tabulate(set(zip(cell_type, counts, ent_score)), headers = ['cell type', 'number of cells', 'mean entropy']))
